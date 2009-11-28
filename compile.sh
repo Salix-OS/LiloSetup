d4349c26c382d2c14530a0b42f888ec5b7cf33e1
Cyrille Pontvieux <jrdATenialisDOTnet>
2009-11-28 22:59:02 +0000

    UPD: current path in lilosetup and salix-live-installer scripts UPD: script to make a txz package in lilosetup and salix-live-installer UPD: scripts in salix-live. ADD: script to install the live ISO on USB.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 for i in `ls locale/*.po`;do
 	echo "Compiling `echo $i|sed "s|locale/||"`"
 	msgfmt $i -o `echo $i |sed "s/lilosetup-//"|sed "s/.po//"`.mo
---
+++ maketxz.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $(dirname $0)
+./compile.sh
+mkdir -p pkg
+export DESTDIR=$PWD/pkg
+./install.sh
+VER=$(grep 'version=' src/lilosetup | cut -d\' -f2)
+cd pkg
+makepkg -l y -c n ../lilosetup-$VER-noarch-1plb.txz
+cd ..
+rm -rf pkg

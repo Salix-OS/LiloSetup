8158bdfc097996bf1b6c95cf06f4e0b05a0b501a
George Vlahavas <vlahavasATgmailDOTcom>
2010-03-24 21:18:31 +0000

    Added creation for .desktop file from .desktop.in file

--- compile.sh
+++ compile.sh
@@ -6,3 +6,4 @@ for i in `ls po/*.po`;do
 	echo "Compiling `echo $i|sed "s|po/||"`"
 	msgfmt $i -o `echo $i |sed "s/lilosetup-//"|sed "s/.po//"`.mo
 done
+intltool-merge po/ -d -u src/$i.desktop.in src/$i.desktop

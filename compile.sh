d975997c375e8d38f6575c731154a1437ee4f2e2
George Vlahavas <vlahavasATgmailDOTcom>
2010-03-24 21:04:01 +0000

    Renamed locale dir to po

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-for i in `ls locale/*.po`;do
-	echo "Compiling `echo $i|sed "s|locale/||"`"
+for i in `ls po/*.po`;do
+	echo "Compiling `echo $i|sed "s|po/||"`"
 	msgfmt $i -o `echo $i |sed "s/lilosetup-//"|sed "s/.po//"`.mo
 done

27b60fa74866821be4bc1e888af49a2dedfa5177
Pierrick Le Brun <pierrickATsimplynuxDOTnet>
2010-10-25 03:30:52 +0000

    Updated lilosetup translation strings.

--- update-translations.sh
+++ update-translations.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 intltool-extract --type="gettext/ini" src/lilosetup.desktop.in
+intltool-extract --type="gettext/ini" src/lilosetup-kde.desktop.in
 
 xgettext --from-code=utf-8 \
 	-L Glade \
@@ -13,8 +14,9 @@ xgettext --from-code=utf-8 \
 	-o po/lilosetup.pot \
 	src/lilosetup.py
 xgettext --from-code=utf-8 -j -L C -kN_ -o po/lilosetup.pot src/lilosetup.desktop.in.h
+xgettext --from-code=utf-8 -j -L C -kN_ -o po/lilosetup.pot src/lilosetup-kde.desktop.in.h
 
-rm src/lilosetup.desktop.in.h
+rm src/lilosetup.desktop.in.h src/lilosetup-kde.desktop.in.h
 
 cd po
 for i in `ls *.po`; do
